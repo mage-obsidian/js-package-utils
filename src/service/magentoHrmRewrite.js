626a9596c41c738b2001352aaf58341932851f93
Jeanmarcos Juarez <janmarcoj@gmail.com>
2024-12-12 13:29:52 -0400

    Enhance file path handling in magentoHrmRewrite service with dynamic MIME type setting

--- src/service/magentoHrmRewrite.js
+++ src/service/magentoHrmRewrite.js
@@ -12,7 +12,26 @@ export default () => {
 
                 if (matchedKey) {
                     const filePath = components[matchedKey];
-                    const suffix = matchedKey === 'lib/vue' ? '/' : '/@fs';
+                    let suffix = '/@fs';
+                    if (matchedKey === 'lib/vue') {
+                        suffix = '/';
+                    } else if (matchedKey.startsWith('lib/')) {
+                        suffix = '/@fs';
+                        const fileExtension = filePath.split('.').pop();
+                        let mimeType = 'application/node';
+                        if (
+                            fileExtension === 'cjs' ||
+                            fileExtension === 'mjs' ||
+                            fileExtension === 'js'
+                        ) mimeType = 'application/javascript';
+                        if (fileExtension === 'css') mimeType = 'text/css';
+                        if (fileExtension === 'json') mimeType = 'application/json';
+                        if (fileExtension === 'html') mimeType = 'text/html';
+
+                        res.setHeader('Content-Type', mimeType);
+                    } else {
+                        suffix = '/@fs';
+                    }
                     req.url = `${suffix}${filePath}`;
                     next();
                 } else {
