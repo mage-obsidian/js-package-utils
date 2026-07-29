c12f5d6a465566b8dba3a047ab5967c9ed8c5882
Jeanmarcos Juarez <janmarcoj@gmail.com>
2026-07-29 00:45:00 -0400

    build(lint): stop linting the build output

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,7 @@ export default [
     {
         ignores: [
             "node_modules/**",
+            "dist/**",
             ".precompiled/**",
             "src/__tests__/magento_scenarios/**",
             "src/__tests__/fixtures/**",
